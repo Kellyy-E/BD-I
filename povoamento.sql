a04a55383997a1b02030a6a055553efde0e2d996
kelly <eurikellyluiza@gmail.com>
2026-06-20 19:42:13 -0300

    triggers de bloqueio no emprestimo, multa e controle de eventos

--- povoamento.sql
+++ povoamento.sql
@@ -1,10 +1,8 @@
--- 1. CARGOS DOS FUNCIONÁRIOS
 INSERT INTO cargo_funcionario (nome_cargo) VALUES 
 ('Atendimento'),
 ('Catalogação'),
 ('Digitalização');
 
--- 2. USUÁRIOS INTERNOS (Base para Alunos, Professores e Funcionários)
 INSERT INTO usuario_interno (cpf, nome_completo, email_institucional, telefone, endereco) VALUES
 ('11122233344', 'Carlos Augusto Silva', 'carlos.aluno@ufpi.edu.br', '89999112233', 'Rua 24 de Janeiro, 120 - Centro'),
 ('22233344455', 'Mariana Costa Sousa', 'mariana.aluna@ufpi.edu.br', '89999223344', 'Av. Nossa Senhora de Fátima, 450'),
@@ -15,7 +13,6 @@ INSERT INTO usuario_interno (cpf, nome_completo, email_institucional, telefone, 
 ('77788899900', 'Antônia Alves Rocha', 'antonia.func@ufpi.edu.br', '89999778899', 'Bairro Aerolândia');
 
 
--- 3. SUBCLASSES DE USUÁRIOS (Herança)
 INSERT INTO aluno (cpf, numero_matricula) VALUES 
 ('11122233344', '2023901234'),
 ('22233344455', '2024905678'),
@@ -29,37 +26,31 @@ INSERT INTO funcionario (cpf, turno_work, id_cargo) VALUES
 ('66677788899', 'Manhã', 1), -- Atendimento
 ('77788899900', 'Tarde', 2); -- Catalogação
 
--- 4. PARTICIPANTES EXTERNOS (Para os Eventos)
 INSERT INTO participante_external (cpf, chave_convite_unica, nome_completo, email_contato, instituicao_origem) VALUES
 ('88899900011', 'CONV-24-XTZ', 'Marcos Viana Oliveira', 'marcos.viana@gmail.com', 'IFPI Campus Picos'),
 ('99900011122', 'CONV-24-ABC', 'Juliana de Castro Reis', 'ju.castro@hotmail.com', 'UESPI');
 
--- 5. AUTORES
 INSERT INTO autor (nome_autor) VALUES
 ('Ramez Elmasri'),
 ('Shamkant B. Navathe'),
 ('Robert C. Martin'),
 ('Abraham Silberschatz');
 
--- 6. OBRAS (Catálogo Geral)
 INSERT INTO obra (titulo, editora, ano_publicacao, idioma, num_paginas_ou_duracao) VALUES
 ('Sistemas de Banco de Dados', 'Pearson', 2011, 'Português', 700),
 ('Clean Code', 'Alta Books', 2009, 'Inglês', 464),
 ('Sistemas Operacionais Modernos', 'Pearson', 2015, 'Português', 600),
 ('Algoritmos e Estruturas de Dados', 'LTC', 2018, 'Português', 400);
 
--- 7. MAPEAMENTO MUITOS-PARA-MUITOS (Obras e Autores)
 INSERT INTO obra_autor (id_obra, id_author) VALUES
 (1, 1),
 (1, 2), 
 (2, 3), 
 (3, 4); 
 
--- 8. EBOOKS (Relacionamento 1:1 com Obra)
 INSERT INTO ebook (id_obra, link_acesso) VALUES
 (2, 'https://biblioteca.ufpi.br/ebooks/cleancode.pdf');
 
--- 9. EXEMPLARES FÍSICOS (Inventário das Obras Físicas)
 INSERT INTO exemplar_fisico (id_obra, localizacao_biblioteca, status_disponibilidade) VALUES
 (1, 'Estante 04, Prateleira B', 'Disponível'),
 (1, 'Estante 04, Prateleira B', 'Emprestado'), 
@@ -67,7 +58,6 @@ INSERT INTO exemplar_fisico (id_obra, localizacao_biblioteca, status_disponibili
 (3, 'Estante 02, Prateleira A', 'Disponível'),
 (4, 'Estante 01, Prateleira D', 'Disponível');
 
--- 10. DISPOSITIVOS DE ACESSO
 INSERT INTO dispositivo_acesso (tipo_dispositivo, sistema_operacional, status_disponibilidade) VALUES
 ('Computador', 'Ubuntu Linux 24.04', 'Disponível'),
 ('Computador', 'Ubuntu Linux 24.04', 'Em Manutenção'),
@@ -79,28 +69,24 @@ INSERT INTO uso_dispositivo (id_dispositivo, cpf_usuario, horario_inicio, horari
 (2, '22233344455', '2026-06-18 14:00:00', NULL),
 (3, '66677788899','2026-06-15 08:00:00', '2026-06-15 10:00:00');         
 
--- 11. EMPRÉSTIMOS
 INSERT INTO emprestimo (cpf_usuario, id_exemplar, data_retirada, data_prevista_devolucao, data_efetiva_devolucao, multa_atraso) VALUES
 ('11122233344', 1, '2026-05-10', '2026-05-24', '2026-05-20', 0.00), 
 ('11122233344', 2, '2026-06-01', '2026-06-15', NULL, 0.00),        
-('22233344455', 4, '2026-05-01', '2026-05-15', '2026-05-20', 15.50),
+('22233344455', 4, '2026-05-01', '2026-05-15', '2026-05-20', 0.00),
 ('22233344455', 1, '2026-02-10', '2026-02-24', '2026-02-20', 0.00), 
 ('22233344455', 2, '2026-03-01', '2026-03-15', '2026-03-12', 0.00),
 ('22233344455', 3, '2026-04-05', '2026-04-19', '2026-04-15', 0.00); 
 
 
--- 12. INFRAESTRUTURA (Locais dos Eventos)
 INSERT INTO local_evento (capacidade_maxima, predio, andar, bloco) VALUES
 (50, 'Bloco de Sistemas', '1º Andar', 'Auditório B'),
 (30, 'Biblioteca Central', 'Térreo', 'Sala de Estudos 01');
 
--- 13. EVENTOS
 INSERT INTO evento (id_local, tipo_evento, data_realizacao, horario_inicio, horario_fim, publico_alvo, tema_abordado) VALUES
 (1, 'Palestra', '2026-06-24', '09:00:00', '11:00:00', 'Alunos de TI', 'Introdução à Álgebra Relacional e Bancos de Dados NoSQL'),
 (2, 'Club de Leitura', '2026-06-25', '14:00:00', '16:00:00', 'Comunidade Geral', 'Análise Crítica da Obra Dom Casmurro de Machado de Assis'),
 (1, 'Palestra', '2026-06-26', '15:00:00', '17:30:00', 'Professores e Pesquisadores', 'O Impacto da Inteligência Artificial na Educação Superior');
 
--- 14. INSCRIÇÕES NOS EVENTOS 
 INSERT INTO inscricao_evento (id_evento, cpf_participante, data_inscricao) VALUES
 (1, '11122233344', '2026-06-10'), 
 (2, '11122233344', '2026-06-11'), 
--- triggers.sql
+++ triggers.sql
@@ -1,4 +1,6 @@
--- **** ATUALIZAÇÃO STATUS DE UM EXEMPLAR: PÓS EMPRÉSTIMO *****
+--**************
+-- **** 1 - ATUALIZAÇÃO STATUS DE UM EXEMPLAR: PÓS EMPRÉSTIMO *****
+--**************
 
 --FUNÇÃO
 CREATE OR REPLACE FUNCTION atualizar_status_exemplar()
@@ -18,7 +20,9 @@ AFTER INSERT ON emprestimo
 FOR EACH ROW
 EXECUTE FUNCTION atualizar_status_exemplar();
 
--- **** ATUALIZAÇÃO STATUS DE UM EXEMPLAR: PÓS DEVOLUÇÃO *****
+--**************
+-- **** 2 - ATUALIZAÇÃO STATUS DE UM EXEMPLAR: PÓS DEVOLUÇÃO *****
+--**************
 
 -- FUNÇÃO
 CREATE OR REPLACE FUNCTION registrar_devolucao_exemplar()
@@ -39,3 +43,97 @@ CREATE TRIGGER trg_entrada_exemplar
 AFTER UPDATE ON emprestimo
 FOR EACH ROW
 EXECUTE FUNCTION registrar_devolucao_exemplar();
+
+
+--**************
+-- **** 3 - BLOQUEIO DE EMPRESTIMO DE EXEMPLAR EMPRESTADO *****
+--**************
+
+-- FUNCAO
+CREATE OR REPLACE FUNCTION checar_disponibilidade_exemplar()
+RETURNS TRIGGER AS $$
+DECLARE
+    status_atual VARCHAR(20);
+BEGIN
+    SELECT status_disponibilidade INTO status_atual
+    FROM exemplar_fisico
+    WHERE id_exemplar = NEW.id_exemplar;
+    
+    IF status_atual != 'Disponível' THEN
+        RAISE EXCEPTION 'Erro: O exemplar % não pode ser emprestado. O status atual é: %', NEW.id_exemplar, status_atual;
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- TRIGGER 
+CREATE TRIGGER trg_checar_disponibilidade
+BEFORE INSERT ON emprestimo
+FOR EACH ROW
+EXECUTE FUNCTION checar_disponibilidade_exemplar();
+
+
+--**************
+-- **** 4 - ATUALIZAÇÃO DA MULTA POR ATRASO *****
+--**************
+
+-- FUNÇÃO
+CREATE OR REPLACE FUNCTION calcular_multa_atraso()
+RETURNS TRIGGER AS $$
+DECLARE
+    dias_atraso INT;
+    valor_diaria NUMERIC := 1.00; 
+BEGIN
+    IF NEW.data_efetiva_devolucao IS NOT NULL AND OLD.data_efetiva_devolucao IS NULL THEN
+        
+        IF NEW.data_efetiva_devolucao > NEW.data_prevista_devolucao THEN
+            dias_atraso := NEW.data_efetiva_devolucao - NEW.data_prevista_devolucao;
+            
+            NEW.multa_atraso := dias_atraso * valor_diaria;
+        ELSE
+            NEW.multa_atraso := 0.00;
+        END IF;
+        
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- TRIGGER
+CREATE TRIGGER trg_calcular_multa
+BEFORE UPDATE ON emprestimo
+FOR EACH ROW
+EXECUTE FUNCTION calcular_multa_atraso();
+
+--**************
+-- **** 5 - PRECAUÇÃO CONTRA "CHOQUE" DE EVENTOS *****
+--**************
+
+-- FUNÇÃO
+CREATE OR REPLACE FUNCTION checar_conflito_horario_evento()
+RETURNS TRIGGER AS $$
+DECLARE
+    quantidade_conflitos INT;
+BEGIN
+    SELECT COUNT(*) INTO quantidade_conflitos
+    FROM evento
+    WHERE id_local = NEW.id_local 
+      AND data_realizacao = NEW.data_realizacao
+      AND (NEW.horario_inicio < horario_fim AND NEW.horario_fim > horario_inicio)
+      AND id_evento != COALESCE(NEW.id_evento, -1);
+
+    IF quantidade_conflitos > 0 THEN
+        RAISE EXCEPTION 'Erro de Agendamento: Já existe um evento reservado para o local % na data % neste intervalo de horário.', NEW.id_local, NEW.data_realizacao;
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- TRIGGER
+CREATE TRIGGER trg_evitar_conflito_evento
+BEFORE INSERT OR UPDATE ON evento
+FOR EACH ROW
+EXECUTE FUNCTION checar_conflito_horario_evento();
